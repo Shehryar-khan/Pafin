0c9988b07d91dc63f5a5848d77d50c77dda20529
Shehryar-khan <shehryar_tanoli@outlook.com>
2023-11-25 12:05:11 +0900

    docker-compose-link-issue-fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      synchronize: true, //should be false in production,migrations should be used
+      logging: false,
+      autoLoadEntities: true,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
